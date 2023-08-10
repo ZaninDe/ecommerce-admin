9952418db9bfcf5997178c7683d0644091847e80
ZaninDe <gabriel.zaninde@gmail.com>
2023-08-10 15:39:42 -0300

    add sizes entity

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { CategoryColumn } from './columns'
+import { SizeColumn } from './columns'
 import { Button } from '@/components/ui/button'
 import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react'
 import { toast } from 'react-hot-toast'
@@ -17,7 +17,7 @@ import { useState } from 'react'
 import { AlertModal } from '@/components/modals/alert-modal'
 
 interface CellActionsProps {
-  data: CategoryColumn
+  data: SizeColumn
 }
 
 export const CellAction = ({ data }: CellActionsProps) => {
@@ -29,20 +29,18 @@ export const CellAction = ({ data }: CellActionsProps) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id)
-    toast.success('Category id copied to the clipboard')
+    toast.success('Size id copied to the clipboard')
   }
 
   const onDelete = async () => {
     try {
       setLoading(true)
 
-      await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`)
       router.refresh()
-      toast.success('Category deleted.')
+      toast.success('Size deleted.')
     } catch (error) {
-      toast.error(
-        'Make sure you removed all products using this category first',
-      )
+      toast.error('Make sure you removed all products using this size first')
     } finally {
       setLoading(false)
       setOpen(false)
@@ -70,9 +68,7 @@ export const CellAction = ({ data }: CellActionsProps) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/categories/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
